409c3ce90a982d49d373339a2b02b6ce157ad2f0
eric_bresie <ebresie@gmail.com>
2020-12-27 09:20:47 -0600

    Added histograph and graph config examples

---
+++ DataCamp/DataCamp-Intermediate/historyExample.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+year = [ 1950, 1970, 1990, 2010] 
+pop = [2.519, 3.692, 5.263, 6.972] 
+# historgraph without any bin count or other parameters
+plt.hist(year)
+plt.show()
+plt.cl()
+
+plt.hist(pop)
+plt.show()
